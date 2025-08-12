51c3288b98374d9c251cdeb556e939081c72c9cc
Amit Bisht <amit.bisht@dishtv.in>
2025-08-12 17:44:03 +0530

    mssg

--- module/main.tf
+++ module/main.tf
@@ -1,17 +1,17 @@
-module "kuku" {
+module "kuku33" {
 source = "../infra"
-res_grp = "inni"
+res_grp = "inni33"
 res_loc = "CentralIndia"
 }
 
-module "chichi" {
+module "chichi33" {
 source = "../infra"
-res_grp = "shusi"
+res_grp = "shusi33"
 res_loc = "CentralIndia"
 }
 
-module "chichi3" {
+module "chichi33" {
 source = "../infra"
-res_grp = "shus3i"
+res_grp = "shusi33"
 res_loc = "CentralIndia"
 }
