e9a175fcab7f61e8d3e7bf855f0bfeffb1791824
Amitish <58432032+Amitish@users.noreply.github.com>
2025-08-12 15:22:57 +0530

    Tehni (#12)
    * Set up CI with Azure Pipelines (#10)
    
    [skip ci]
    
    * new rg added
    
    * 3
    
    ---------
    
    Co-authored-by: Amit Bisht <amit.bisht@dishtv.in>

--- module/main.tf
+++ module/main.tf
@@ -3,3 +3,15 @@ source = "../infra"
 res_grp = "inni"
 res_loc = "CentralIndia"
 }
+
+module "chichi" {
+source = "../infra"
+res_grp = "shusi"
+res_loc = "CentralIndia"
+}
+
+module "chichi3" {
+source = "../infra"
+res_grp = "shus3i"
+res_loc = "CentralIndia"
+}
